acad09113b3a0a21295569748bfd93f64a4003cf
mahonya333 <mahonya333@gmail.com>
2020-10-25 16:46:08 +0200

    All sections are made except (header) and (travels__filter)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 $(function () {
-    $('.reviews__slider').slick({
+    $('.reviews-slider').slick({
         dots: false,
         infinite: true,
         arrows: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplaySpeed: 5000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/slider/arrow-left.svg" alt="">&lt;</button>',
         nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/slider/arrow-right.svg" alt="">&gt;</button>',
         responsive: [
@@ -15,6 +14,7 @@ $(function () {
               breakpoint: 1024,
               settings: {
                 arrows: false,
+                dots: true
               }
             },
         ]
@@ -22,22 +22,20 @@ $(function () {
     $(function() {
         $('input[type="text"],select').styler();
     });
-    $(".reviews__slider-rate").rateYo({
+    $(".reviews-slider__rate").rateYo({
         rating: 4.5,
         // readOnly: true,
         spacing : "5px",
         starWidth: "20px", 
         ratedFill: "#faa31a",
-        // normalFill: "#fff"
+        readOnly: true
     });
-    $(".travels__item-rate").rateYo({
+    $(".travels-item__rate").rateYo({
         rating: 4,
-        // readOnly: true,
         spacing : "5px",
         starWidth: "20px",
         ratedFill: "#ffea00",
         normalFill: "#e3e3e3"   
-        // normalFill: "#fff"
     });
     $('.header__btn-menu').on('click',function(){
         $('.header__navigation ul').slideToggle();
