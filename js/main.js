24d4f86088e65ce38c178f16282a05458da9c0a2
mahonya333 <mahonya333@gmail.com>
2020-11-01 20:11:36 +0200

    project completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+
     $('.reviews-slider').slick({
         dots: false,
         infinite: true,
@@ -7,37 +8,63 @@ $(function () {
         slidesToScroll: 1,
         autoplaySpeed: 5000,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/slider/arrow-left.svg" alt="">&lt;</button>',
-        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/slider/arrow-right.svg" alt="">&gt;</button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev">&lt;</button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next">&gt;</button>',
         responsive: [
             {
-              breakpoint: 1024,
-              settings: {
-                arrows: false,
-                dots: true
-              }
+                breakpoint: 1024,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
             },
         ]
     });
-    $(function() {
+    $(function () {
         $('input[type="text"],select').styler();
     });
     $(".reviews-slider__rate").rateYo({
         rating: 4.5,
         // readOnly: true,
-        spacing : "5px",
-        starWidth: "20px", 
+        spacing: "5px",
+        starWidth: "20px",
         ratedFill: "#faa31a",
         readOnly: true
     });
     $(".travels-item__rate").rateYo({
         rating: 4,
-        spacing : "5px",
+        spacing: "5px",
         starWidth: "20px",
         ratedFill: "#ffea00",
-        normalFill: "#e3e3e3"   
+        normalFill: "#e3e3e3"
     });
-    $('.header__btn-menu').on('click',function(){
-        $('.header__navigation ul').slideToggle();
+    $('.btn-burger-menu').on('click', function () {
+        $('.navigation__list').slideToggle();
     });
+
+});
+
+let navigationHeight = document.getElementById('navigation').offsetHeight;
+
+function navigateScroll(elem) {
+    let target = $(elem).attr('href').split('#');/*Значение атрибута href разделяем на значения до знака "#" и после него и помещаем значения в массив target*/
+    let targetAnchor = "#" + target[1];/*Значение якоря к которому должна вести ссылка*/
+    $('.navigation__list').slideToggle();
+    $([document.documentElement, document.body]).animate({/*Делаем плавный скролл к якорной ссылке с отступом от верха страницы в размере высоты шапки*/
+        scrollTop: $(targetAnchor).offset().top - navigationHeight
+    }, 1000);
+}
+
+// scroll body to 0px on click
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
+$('#back-to-top').click(function () {
+    $('body, html').animate({
+        scrollTop: 0
+    }, 800);
 });
